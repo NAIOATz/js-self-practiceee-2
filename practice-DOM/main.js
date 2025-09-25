f6cc01579b39134e1c78e53c5c2a2b39f5d586f4
Surapoj <surapoj.thon@kmutt.ac.th>
2025-09-25 11:59:44 +0700

    add one element and content

--- practice-DOM/main.js
+++ practice-DOM/main.js
@@ -1,2 +1,14 @@
 // get list by ID
 const listId = document.getElementById("list")
+// get all <li> in <ul>
+const allLists = listId.children
+// console.log content in <li>
+Array.from(allLists).forEach(list => console.log(list.textContent))
+// create new <li>
+const newList = document.createElement('li')
+// add content to newList
+newList.textContent = "AI"
+// put newList to listId
+listId.append(newList)
+console.log("----------------------------------------")
+Array.from(allLists).forEach(list => console.log(list.textContent))
